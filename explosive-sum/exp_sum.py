b0fc8b07e0824b1c4821c934cca3e2424adef018
Erioifpud <erioifpud@zuo.si>
2017-12-29 23:18:17 +0800

    edited explosive sum

--- explosive-sum/exp_sum.py
+++ explosive-sum/exp_sum.py
@@ -1,5 +1,10 @@
+'''
 def p(n1, n2):
-    if n1 == 1 or n2 == 1:
+    if n1 < 0:
+        return 0
+    elif n1 == 0:
+        return 1
+    elif n1 == 1 or n2 == 1:
         return 1
     elif n1 < n2:
         return p(n1, n1)
@@ -11,3 +16,38 @@ def p(n1, n2):
 def exp_sum(n):
     #your code here
     return p(n, n)
+'''
+
+import time
+def p(n1, n2, buf):
+    if buf.get(str(n1) + '|' + str(n2)):
+        return buf.get(str(n1) + '|' + str(n2))
+    else:
+        if n1 < 0:
+            return 0
+        elif n1 == 0:
+            return 1
+        elif n1 == 1 or n2 == 1:
+            return 1
+        elif n1 < n2:
+            a = p(n1, n1, buf)
+            buf[str(n1) + '|' + str(n1)] = a
+            return a
+        elif n1 == n2:
+            a = p(n1, n1 - 1, buf)
+            buf[str(n1) + '|' + str(n1 - 1)] = a
+            return 1 + a
+        else:
+            a, b = p(n1 - n2, n2, buf), p(n1, n2 - 1, buf)
+            buf[str(n1 - n2) + '|' + str(n2)] = a
+            buf[str(n1) + '|' + str(n2 - 1)] = b
+            return a + b
+
+def exp_sum(n):
+    #your code here
+    return p(n, n, {})
+
+print(time.time() * 1000)
+exp_sum(100)
+print()
+print(time.time() * 1000)
